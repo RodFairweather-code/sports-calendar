f6bc20227d64e7ccb51b1b56300e9be1e30087cc
Rod Fairweather <Rod.FAirweather@gmail.com>
2026-05-06 17:17:32 +0100

    Initial data

---
+++ src/seedData.js
@@ -0,0 +1,50 @@
+const PLATFORMS = [
+  { id: 'plat_seed_1', name: 'BBC',        defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
+  { id: 'plat_seed_2', name: 'ITV Ealing', defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
+  { id: 'plat_seed_3', name: 'Sky MCR',    defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
+  { id: 'plat_seed_4', name: 'TAMS',       defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
+  { id: 'plat_seed_5', name: 'NBCU',       defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
+  { id: 'plat_seed_6', name: 'Reuters',    defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
+]
+
+const PATTERNS = [
+  {
+    id: 'pat_seed_1',
+    name: '2 Cam Lower League',
+    cameramen: 2, evsOperator: false,
+    crewFrom: -2, crewUntil: 2,
+    incomingVideoLines: 4, outgoingVideoLines: 2,
+    videoFrom: -2, videoUntil: 2,
+    incomingAudioLines: 6, incomingTalkbackLines: 0, outgoingTalkbackLines: 4,
+    audioFrom: -2, audioUntil: 2,
+  },
+  {
+    id: 'pat_seed_2',
+    name: '4 Cam Middle League',
+    cameramen: 4, evsOperator: false,
+    crewFrom: -3, crewUntil: 2,
+    incomingVideoLines: 8, outgoingVideoLines: 4,
+    videoFrom: -2, videoUntil: 2,
+    incomingAudioLines: 8, incomingTalkbackLines: 0, outgoingTalkbackLines: 6,
+    audioFrom: -2, audioUntil: 2,
+  },
+  {
+    id: 'pat_seed_3',
+    name: '8 Cam Feature Match',
+    cameramen: 8, evsOperator: false,
+    crewFrom: -4, crewUntil: 3,
+    incomingVideoLines: 10, outgoingVideoLines: 4,
+    videoFrom: -3, videoUntil: 2,
+    incomingAudioLines: 12, incomingTalkbackLines: 0, outgoingTalkbackLines: 8,
+    audioFrom: -3, audioUntil: 2,
+  },
+]
+
+export function seedLocalStorage() {
+  if (!localStorage.getItem('admin_platforms')) {
+    localStorage.setItem('admin_platforms', JSON.stringify(PLATFORMS))
+  }
+  if (!localStorage.getItem('admin_patterns')) {
+    localStorage.setItem('admin_patterns', JSON.stringify(PATTERNS))
+  }
+}
