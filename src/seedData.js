dff7c1f0200957ecfb58717066caa4de838726c1
Rod Fairweather <Rod.FAirweather@gmail.com>
2026-05-08 13:36:32 +0100

    Tech 2

--- src/seedData.js
+++ src/seedData.js
@@ -1,4 +1,4 @@
-const SEED_VERSION = 11
+const SEED_VERSION = 12
 
 const PLATFORMS = [
   { id: 'plat_seed_1', name: "BBC 1", defaultIncomingLine: '', defaultOutgoingLine: '', fourWires: 0, feedRouting: '', mcrPhone: '', editorialPhone: '' },
@@ -10,56 +10,7 @@ const PLATFORMS = [
 ]
 
 const PATTERNS = [
-  {
-    id: 'pat_seed_1',
-    name: "2 Cam Lower League",
-    cameramen: 2, evsOperator: false,
-    crewFrom: -2, crewUntil: 2,
-    incomingVideoLines: 4, outgoingVideoLines: 2,
-    videoFrom: -2, videoUntil: 2,
-    incomingAudioLines: 6, incomingTalkbackLines: 0, outgoingTalkbackLines: 4,
-    audioFrom: -2, audioUntil: 2,
-  },
-  {
-    id: 'pat_seed_2',
-    name: "4 Cam middle League",
-    cameramen: 4, evsOperator: false,
-    crewFrom: -3, crewUntil: 2,
-    incomingVideoLines: 8, outgoingVideoLines: 4,
-    videoFrom: -2, videoUntil: 2,
-    incomingAudioLines: 8, incomingTalkbackLines: 0, outgoingTalkbackLines: 6,
-    audioFrom: -2, audioUntil: 2,
-  },
-  {
-    id: 'pat_seed_3',
-    name: "8 Cam Feature Match",
-    cameramen: 8, evsOperator: false,
-    crewFrom: -4, crewUntil: 3,
-    incomingVideoLines: 10, outgoingVideoLines: 4,
-    videoFrom: -3, videoUntil: 2,
-    incomingAudioLines: 12, incomingTalkbackLines: 0, outgoingTalkbackLines: 8,
-    audioFrom: -3, audioUntil: 2,
-  },
-  {
-    id: 'pat_seed_4',
-    name: "250 Tennis",
-    cameramen: 0, evsOperator: false,
-    crewFrom: 0, crewUntil: 0,
-    incomingVideoLines: 4, outgoingVideoLines: 2,
-    videoFrom: -2, videoUntil: 2,
-    incomingAudioLines: 6, incomingTalkbackLines: 0, outgoingTalkbackLines: 4,
-    audioFrom: -2, audioUntil: 2,
-  },
-  {
-    id: 'pat_seed_5',
-    name: "Grand Slam Tennis",
-    cameramen: 1, evsOperator: false,
-    crewFrom: -2, crewUntil: 2,
-    incomingVideoLines: 8, outgoingVideoLines: 4,
-    videoFrom: -2, videoUntil: 2,
-    incomingAudioLines: 12, incomingTalkbackLines: 0, outgoingTalkbackLines: 8,
-    audioFrom: -2, audioUntil: 2,
-  },
+
 ]
 
 const STAFF = {
